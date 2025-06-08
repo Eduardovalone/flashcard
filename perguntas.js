780c9522afbea5d3b12f7b9ae751fe92a0ea0f17
Eduardovalone <eduardovalone@gmail.com>
2025-06-08 13:03:59 +0000

    aula 6

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'The Last Of US',
+    'Em que ano se passa a maior parte da história?',
+    'Em 2023, 20 anos após o surto em 2003.'
+)
+
+criaCartao(
+    'The Last Of Us',
+    'Por que Ellie é tão importante na trama?',
+    'Porque ela é imune à infecção do Cordyceps.'
+)
+
+criaCartao(
+    'The Last Of Us',
+    'A série é baseada em qual outro tipo de mídia?',
+    'Em um videogame lançado originalmente para PlayStation 3 em 2013.'
+)
